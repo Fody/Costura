30fb47dab393a2eee202a79b6f40c6e998373efb
Simon Cropp <simon.cropp@gmail.com>
2013-03-24 17:17:44 +1100

    version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Costura")]
 [assembly: AssemblyProduct("Costura")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
