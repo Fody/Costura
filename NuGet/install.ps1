6bce8e2b8577808b74049d6915b881d5629b1900
Simon Cropp <simon.cropp@gmail.com>
2013-07-09 22:19:58 +1000

    fix VS guid

--- NuGet/install.ps1
+++ NuGet/install.ps1
@@ -79,6 +79,7 @@ function UnlockWeaversXml($project)
     $fodyWeaversProjectItem = $project.ProjectItems.Item("FodyWeavers.xml");
     if ($fodyWeaversProjectItem)
     {
+        $fodyWeaversProjectItem.Open("{7651A701-06E5-11D1-8EBD-00A0C90F26EA}")
         $fodyWeaversProjectItem.Save()
     }   
 }
