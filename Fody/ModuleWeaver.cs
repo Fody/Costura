737b0a019048428588ebe4ea2b9190030c5ae811
Simon Cropp <simon.cropp@gmail.com>
2013-08-10 00:12:38 +1000

    fix for #25

--- Fody/ModuleWeaver.cs
+++ Fody/ModuleWeaver.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Mono.Cecil;
 
-public partial  class ModuleWeaver
+public partial class ModuleWeaver
 {
     public Action<string> LogInfo { get; set; }
     public ModuleDefinition ModuleDefinition { get; set; }
     public List<string> ReferenceCopyLocalPaths { get; set; }
     public IAssemblyResolver AssemblyResolver { get; set; }
+    public string AssemblyFilePath { get; set; }
 
 
     public ModuleWeaver()
@@ -17,6 +19,8 @@ public partial  class ModuleWeaver
 
     public void Execute()
     {
+        var intermediateOutputPath = Path.GetDirectoryName(AssemblyFilePath);
+        LogInfo("IntermediateOutputPath resolved to :" + intermediateOutputPath);
         ReadConfig();
         FindMsCoreReferences();
 
