7b046c064ff6ee36b16b4b10103ec6402b8ea9c4
Justin Thirkell <justin.thirkell@xero.com>
2013-12-16 12:45:25 +1300

    bumped version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Costura")]
 [assembly: AssemblyProduct("Costura")]
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyFileVersion("0.6.0")]
+[assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyFileVersion("0.6.1")]
