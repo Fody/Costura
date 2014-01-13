d494646be7665ea37776bfa85fd1b39e5ce1bdb0
lwoehrl <lwoehrl@recom.eu>
2014-01-13 19:14:46 +0100

    Missed +1

--- Fody/NativeResources.cs
+++ Fody/NativeResources.cs
@@ -10,7 +10,7 @@ partial class ModuleWeaver
         {
             if (resource.Name.IndexOf(".costura", StringComparison.OrdinalIgnoreCase) >= 0)
             {
-                resource.Name = resource.Name.Substring(resource.Name.IndexOf(".costura", StringComparison.OrdinalIgnoreCase)).ToLowerInvariant();
+                resource.Name = resource.Name.Substring(resource.Name.IndexOf(".costura", StringComparison.OrdinalIgnoreCase) + 1).ToLowerInvariant();
                 hasUnmanaged = true;
                 checksums.Add(resource.Name, CalculateChecksum(resource.GetResourceStream()));
             }
