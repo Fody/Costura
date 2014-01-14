c49aabcc3e9a0bceaec2e925ba322c0dbf1bee60
Cameron MacFarland <distantcam@gmail.com>
2014-01-14 10:48:40 +0800

    more robust native lib search. ver bump

--- Fody/NativeResources.cs
+++ Fody/NativeResources.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Mono.Cecil;
 
 partial class ModuleWeaver
 {
     void ProcessNativeResources()
     {
+        var unprocessedNameMatch = new Regex(@"^(.*\.)?costura(32|64)\.", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+        var processedNameMatch = new Regex(@"^costura(32|64)\.", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
         foreach (var resource in ModuleDefinition.Resources.OfType<EmbeddedResource>())
         {
-            int costuraHintPosition = resource.Name.IndexOf(".costura", StringComparison.OrdinalIgnoreCase);
-            if (costuraHintPosition >= 0)
+            var match = unprocessedNameMatch.Match(resource.Name);
+            if (match.Success)
             {
-                resource.Name = resource.Name.Substring(costuraHintPosition + 1).ToLowerInvariant();
+                resource.Name = resource.Name.Substring(match.Groups[1].Length).ToLowerInvariant();
                 hasUnmanaged = true;
+            }
+
+            if (processedNameMatch.IsMatch(resource.Name))
+            {
                 checksums.Add(resource.Name, CalculateChecksum(resource.GetResourceStream()));
             }
         }
