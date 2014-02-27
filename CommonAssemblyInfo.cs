46ec9d0273ac32ebf180e67b69c928f7c01ff3b7
Cameron MacFarland <distantcam@gmail.com>
2014-02-27 14:56:13 +0800

    Updated cleanup script to not clean up excluded files.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Costura")]
 [assembly: AssemblyProduct("Costura")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
--- Fody/NugetAssets/commands.psm1
+++ Fody/NugetAssets/commands.psm1
@@ -30,42 +30,77 @@ function Get-MSBuildProject {
 
 function Install-CleanReferencesTarget()
 {
-	$buildProject = Get-MSBuildProject
+    $buildProject = Get-MSBuildProject
 
-	if ($buildProject.Xml.Targets | Where-Object { "CleanReferenceCopyLocalPaths" -contains $_.Name })
-	{
-		Write-Host "Target CleanReferenceCopyLocalPaths already exists." -foregroundcolor Black -backgroundcolor Yellow
+    if ($buildProject.Xml.Targets | Where-Object { "CleanReferenceCopyLocalPaths" -contains $_.Name })
+    {
+        Write-Host "Target CleanReferenceCopyLocalPaths already exists." -foregroundcolor Black -backgroundcolor Yellow
 
-		return
-	}
+        return
+    }
 
-	$target = $buildProject.Xml.AddTarget("CleanReferenceCopyLocalPaths")
+    $usingTask = $buildProject.Xml.AddUsingTask("CosturaCleanup", "`$(MSBuildToolsPath)\Microsoft.Build.Tasks.v4.0.dll", "")
+	$usingTask.TaskFactory = "CodeTaskFactory"
+    $parameterGroup = $usingTask.AddParameterGroup()
+    $configParam = $parameterGroup.AddParameter("Config", "false", "true", "Microsoft.Build.Framework.ITaskItem")
+    $filesParam = $parameterGroup.AddParameter("Files", "false", "true", "Microsoft.Build.Framework.ITaskItem[]")
+	$taskBody = $usingTask.AddUsingTaskBody("true", "<Reference xmlns=`"http://schemas.microsoft.com/developer/msbuild/2003`" Include=`"System.Xml`"/>
+      <Reference xmlns=`"http://schemas.microsoft.com/developer/msbuild/2003`" Include=`"System.Xml.Linq`"/>
+      <Using xmlns=`"http://schemas.microsoft.com/developer/msbuild/2003`" Namespace=`"System`"/>
+      <Using xmlns=`"http://schemas.microsoft.com/developer/msbuild/2003`" Namespace=`"System.IO`"/>
+      <Using xmlns=`"http://schemas.microsoft.com/developer/msbuild/2003`" Namespace=`"System.Xml.Linq`"/>
+      <Code xmlns=`"http://schemas.microsoft.com/developer/msbuild/2003`" Type=`"Fragment`" Language=`"cs`">
+<![CDATA[
+var config = XElement.Load(Config.ItemSpec).Elements(`"Costura`").FirstOrDefault();
 
-	$target.AfterTargets = "AfterBuild;NonWinFodyTarget"
+if (config == null) return true;
 
-	$deleteTask = $target.AddTask("Delete")
+var excludedAssemblies = new List<string>();
+var attribute = config.Attribute(`"ExcludeAssemblies`");
+if (attribute != null)
+    foreach (var item in attribute.Value.Split('|').Select(x => x.Trim()).Where(x => x != string.Empty))
+        excludedAssemblies.Add(item);
+var element = config.Element(`"ExcludeAssemblies`");
+if (element != null)
+    foreach (var item in element.Value.Split(new[] { `"\r\n`", `"\n`" }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).Where(x => x != string.Empty))
+        excludedAssemblies.Add(item);
 
-	$deleteTask.SetParameter("Files", "@(ReferenceCopyLocalPaths->`'`$(OutDir)%(DestinationSubDirectory)%(Filename)%(Extension)`')")
+var filesToCleanup = Files.Select(f => f.ItemSpec).Where(f => !excludedAssemblies.Contains(Path.GetFileNameWithoutExtension(f), StringComparer.InvariantCultureIgnoreCase));
 
-	$buildProject.Save()
+foreach (var item in filesToCleanup)
+  File.Delete(item);
+]]>
+      </Code>")
 
-	Write-Host "Added target CleanReferenceCopyLocalPaths."
+    $target = $buildProject.Xml.AddTarget("CleanReferenceCopyLocalPaths")
+    $target.AfterTargets = "AfterBuild;NonWinFodyTarget"
+    $deleteTask = $target.AddTask("CosturaCleanup")
+    $deleteTask.SetParameter("Config", "FodyWeavers.xml")
+    $deleteTask.SetParameter("Files", "@(ReferenceCopyLocalPaths->`'`$(OutDir)%(DestinationSubDirectory)%(Filename)%(Extension)`')")
+
+    $buildProject.Save()
+
+    Write-Host "Added target CleanReferenceCopyLocalPaths."
 }
 
 function Uninstall-CleanReferencesTarget()
 {
-	$buildProject = Get-MSBuildProject
+    $buildProject = Get-MSBuildProject
 
-	$target = $buildProject.Xml.Targets | Where-Object { "CleanReferenceCopyLocalPaths" -contains $_.Name }
+    $target = $buildProject.Xml.Targets | Where-Object { "CleanReferenceCopyLocalPaths" -contains $_.Name }
+    $usingTask = $buildProject.Xml.UsingTasks | Where-Object { "CosturaCleanup" -contains $_.TaskName }
 
-	if (!$target)
-	{
-		Write-Host "Target CleanReferenceCopyLocalPaths did not exist." -foregroundcolor Black -backgroundcolor Yellow
+    if (!$target)
+    {
+        Write-Host "Target CleanReferenceCopyLocalPaths did not exist." -foregroundcolor Black -backgroundcolor Yellow
 
-		return
-	}
+        return
+    }
 
-	$buildProject.Xml.RemoveChild($target)
+    $buildProject.Xml.RemoveChild($usingTask)
+    $buildProject.Xml.RemoveChild($target)
 
-	Write-Host "Removed target CleanReferenceCopyLocalPaths."
+    $buildProject.Save()
+
+    Write-Host "Removed target CleanReferenceCopyLocalPaths."
 }
