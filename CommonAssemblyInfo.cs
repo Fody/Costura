f50ea660afc9412eaa426a7bd4f693fbe17c6e6a
Cameron MacFarland <distantcam@gmail.com>
2014-03-18 18:29:33 +0800

    Switched to using xdt instead of install script.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Costura")]
 [assembly: AssemblyProduct("Costura")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
