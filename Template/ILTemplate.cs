96b272a18e611999e0fc009722c26a83d980c558
Stefano Candori <kendrix16@yahoo.it>
2014-07-09 16:37:41 +0200

    Fix issue #74

--- Template/ILTemplate.cs
+++ Template/ILTemplate.cs
@@ -36,6 +36,21 @@ static class ILTemplate
         if (assembly == null)
         {
             nullCache.Add(assemblyName, true);
+
+            // We should get here only when we need to load a dependency of a dll we have embedded. If such dependency
+            // it's not already loaded ("Common.ReadExistingAssembly" fails), and it's not in the same path of our application
+            // we let Fusion try to load it. We have to do this because "Common.ReadFromEmbeddedResources" uses Assembly.LoadFile
+            // that doesn't automatically load the dependencies.
+            // From: http://blogs.msdn.com/b/suzcook/archive/2003/09/19/loadfile-vs-loadfrom.aspx
+            //
+            // " LoadFile() has a catch. Since it doesn't use a binding context, its dependencies aren't automatically 
+            // " found in its directory. If they aren't available in the Load context, you would have to subscribe to the 
+            // " AssemblyResolve event in order to bind to them."
+            //
+            // If the dependencies is not found we won't get any StackOverflowException (caused by the call to Assembly.Load
+            // that recursively fire another AssemblyResolve event) because we have already added the requestedAssemblyName
+            // to the nullCache and we'll just skip it next time it's "asked".
+            assembly = Assembly.Load(requestedAssemblyName);
         }
         return assembly;
     }
