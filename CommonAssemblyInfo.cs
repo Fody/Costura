bae9ca3737ad0d8e8f31604c41e2436939e0a352
Cameron MacFarland <distantcam@gmail.com>
2014-11-07 11:23:52 +0800

    Added GitVersionTask

--- CommonAssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Costura")]
-[assembly: AssemblyProduct("Costura")]
-[assembly: AssemblyVersion("1.3.2")]
-[assembly: AssemblyFileVersion("1.3.2")]
