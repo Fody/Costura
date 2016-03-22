c806ad197eab3500dcc2620c71359cbf20aa7b1a
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-03-22 22:59:22 +0100

    Update build script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget https://github.com/quamotion/Stamp/archive/features/managed-verpatch.zip -O Stamp.zip
+wget https://github.com/GitTools/GitVersion/archive/release/4.0.0.zip -O GitVersion.zip
+wget https://github.com/nunit/nunit/releases/download/3.2.0/NUnit-3.2.0.zip -O nunit.zip
+wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -O nuget.exe
+unzip Stamp.zip
+mv Stamp-features-managed-verpatch Stamp
+unzip GitVersion.zip
+mv GitVersion-release-4.0.0/ GitVersion
+unzip nunit.zip -d nunit
+cd GitVersion/src/
+mono ../../nuget.exe restore
+xbuild
+cd ../../
+cd Stamp
+mono ../nuget.exe restore
+xbuild
+cd ..
+
