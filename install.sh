174769fa2576d9b8e13cc841fdd74a5d1987da71
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-03-22 23:07:01 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+rm -rf GitVersion
+rm -rf Stamp
+rm -rf nunit
+rm -f nunit.zip
+rm -f Stamp.zip
+rm -f GitVersion.zip
 wget https://github.com/quamotion/Stamp/archive/features/managed-verpatch.zip -O Stamp.zip
 wget https://github.com/GitTools/GitVersion/archive/release/4.0.0.zip -O GitVersion.zip
 wget https://github.com/nunit/nunit/releases/download/3.2.0/NUnit-3.2.0.zip -O nunit.zip
