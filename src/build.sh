ab055e69521478c6d31ac7a983831ec7279192ef
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-03-23 19:48:41 +0100

    Enable NUnit tests

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 nuget restore Costura.Mono.sln
 cp -r GitVersion/build/NuGetTaskBuild/* packages/GitVersionTask.3.3.0/
-xbuild Costura.Mono.sln
-nunit-console Costura.Tests/bin/Debug/Costura.Tests.dll
+xbuild Costura.Mono.sln /p:DefineConstants=DEBUG;MONO
+mono NUnit-2.6.4/bin/nunit-console.exe Costura.Tests/bin/Debug/Costura.Tests.dll
 
 
--- src/install.sh
+++ src/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 wget https://github.com/GitTools/GitVersion/archive/release/4.0.0.zip -O GitVersion.zip
 wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -O nuget.exe
+wget http://github.com/nunit/nunitv2/releases/download/2.6.4/NUnit-2.6.4.zip
+unzip NUnit-2.6.4.zip
 unzip GitVersion.zip
 mv GitVersion-release-4.0.0/ GitVersion
 unzip nunit.zip -d nunit
