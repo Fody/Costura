a284c014b0850f4159a36e567b139213f10de912
Cameron MacFarland <distantcam@gmail.com>
2017-05-25 15:50:38 +0800

    Simplified approval test names

--- src/Costura.Tests/BaseCosturaTest.cs
+++ src/Costura.Tests/BaseCosturaTest.cs
@@ -1,17 +1,44 @@
 ﻿using ApprovalTests;
 using ApprovalTests.Namers;
+using ApprovalTests.Writers;
 using NUnit.Framework;
 
 public abstract class BaseCosturaTest : BaseCostura
 {
 #if DEBUG
 
+    private class CustomNamer : UnitTestFrameworkNamer
+    {
+        public string AdditionalInfo
+        {
+            get
+            {
+                var additionalInformation = NamerFactory.AdditionalInformation;
+                if (additionalInformation != null)
+                {
+                    NamerFactory.AdditionalInformation = null;
+                    additionalInformation = "." + additionalInformation;
+                }
+                return additionalInformation;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return nameof(BaseCosturaTest.TemplateHasCorrectSymbols) + AdditionalInfo;
+            }
+        }
+    }
+
     [Test, Category("IL")]
     public void TemplateHasCorrectSymbols()
     {
         using (ApprovalResults.ForScenario(Suffix))
         {
-            Approvals.Verify(Decompiler.Decompile(afterAssemblyPath, "Costura.AssemblyLoader"));
+            var text = Decompiler.Decompile(afterAssemblyPath, "Costura.AssemblyLoader");
+            Approvals.Verify(WriterFactory.CreateTextWriter(text), new CustomNamer(), Approvals.GetReporter());
         }
     }
 
