375a95f3e94f0b558683e57bb80d0ca418d9db5c
Simon Cropp <simon.cropp@gmail.com>
2018-04-02 11:04:53 +1000

    fix project names

--- Costura.Fody/Checksums.cs
+++ Costura.Fody/Checksums.cs
@@ -18,26 +18,29 @@ partial class ModuleWeaver
     static string CalculateChecksum(Stream stream)
     {
         using (var bs = new BufferedStream(stream))
+        using (var sha1 = new SHA1CryptoServiceProvider())
         {
-            using (var sha1 = new SHA1CryptoServiceProvider())
+            var hash = sha1.ComputeHash(bs);
+            var formatted = new StringBuilder(2 * hash.Length);
+            foreach (var b in hash)
             {
-                var hash = sha1.ComputeHash(bs);
-                var formatted = new StringBuilder(2 * hash.Length);
-                foreach (var b in hash)
-                {
-                    formatted.AppendFormat("{0:X2}", b);
-                }
-                return formatted.ToString();
+                formatted.AppendFormat("{0:X2}", b);
             }
+
+            return formatted.ToString();
         }
     }
 
     void AddChecksumsToTemplate()
     {
         if (checksumsField == null)
+        {
             return;
+        }
 
         foreach (var checksum in checksums)
+        {
             AddToDictionary(checksumsField, checksum.Key, checksum.Value);
+        }
     }
 }
