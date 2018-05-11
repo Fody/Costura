7842c19b7e6ced892f41f3b7b9ad059a8f94c501
Maxx Wyndham <maxx@maxxwyndham.com>
2018-05-11 17:05:55 +0100

    Replicated code from https://github.com/Fody/Fody/issues/363
    Fixes #272

--- Costura.Tasks/ConfigFileFinder.cs
+++ Costura.Tasks/ConfigFileFinder.cs
@@ -11,24 +11,29 @@ public class ConfigFileFinder
         if (File.Exists(solutionConfigFilePath))
         {
             files.Add(solutionConfigFilePath);
+            logger.LogDebug($"Found path to weavers file '{solutionConfigFilePath}'.");
         }
 
         var projectConfigFilePath = Path.Combine(projectDirectory, "FodyWeavers.xml");
         if (!File.Exists(projectConfigFilePath))
         {
-            throw new Exception(
-                $@"Could not file a FodyWeavers.xml at the project level ({projectConfigFilePath}). Some project types do not support using NuGet to add content files e.g. netstandard projects. In these cases it is necessary to manually add a FodyWeavers.xml to the project. Example content:
-<Weavers>
-<WeaverName/>
-</Weavers>
-");
+            logger.LogDebug($@"Could not file a FodyWeavers.xml at the project level ({projectConfigFilePath}). Some project types do not support using NuGet to add content files e.g. netstandard projects. In these cases it is necessary to manually add a FodyWeavers.xml to the project. Example content:
+  <Weavers>
+    <WeaverName/>
+  </Weavers>
+  ");
         }
-        files.Add(projectConfigFilePath);
+        else
+        {
+            files.Add(projectConfigFilePath);
+            logger.LogDebug($"Found path to weavers file '{projectConfigFilePath}'.");
+        }
 
         if (files.Count == 0)
         {
             // ReSharper disable once UnusedVariable
             var pathsSearched = string.Join("', '", solutionConfigFilePath, projectConfigFilePath);
+            throw new WeavingException($"Could not find path to weavers file. Searched '{pathsSearched}'.");
         }
         return files;
     }
