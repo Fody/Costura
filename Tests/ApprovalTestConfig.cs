a18ab30dbd10845a89feb61532d40de1fe8d7d4a
linraise <5512474+LinRaise@users.noreply.github.com>
2019-04-25 22:51:00 +0800

    Revert "try threaded"
    This reverts commit 28a48b13d2ff0186e593f9ecd13d26c546a6a65e.

--- Tests/ApprovalTestConfig.cs
+++ Tests/ApprovalTestConfig.cs
@@ -2,3 +2,4 @@
 using Xunit;
 
 [assembly: UseReporter(typeof(DiffReporter),typeof(AllFailingTestsClipboardReporter))]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
