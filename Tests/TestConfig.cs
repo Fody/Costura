2ab9d027b8ef3e98dc108009f52f9b6a0544c26f
Simon Cropp <simon.cropp@gmail.com>
2019-08-16 21:39:14 +1000

    .

---
+++ Tests/TestConfig.cs
@@ -0,0 +1,5 @@
+﻿using ApprovalTests.Reporters;
+using Xunit;
+
+[assembly: UseReporter(typeof(DiffReporter),typeof(AllFailingTestsClipboardReporter))]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
