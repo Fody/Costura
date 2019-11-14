5a4a3519b566053c2eb7bdb7259c42b83f10b17b
Geert van Horrik <geert@catenalogic.com>
2019-11-14 12:34:38 +0100

    Fix unit tests

--- src/Costura.Fody.Tests/WeavingHelper.cs
+++ src/Costura.Fody.Tests/WeavingHelper.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using Costura.Fody.Tests;
 using Fody;
 
 public static class WeavingHelper
@@ -11,10 +12,16 @@ public static class WeavingHelper
         var weavingTask = new ModuleWeaver
         {
             Config = XElement.Parse(config),
-            References = string.Join(";", references.Select(r => Path.Combine(Environment.CurrentDirectory, r))),
-            ReferenceCopyLocalPaths = references.Select(r => Path.Combine(Environment.CurrentDirectory, r)).ToList(),
+            References = string.Join(";", references.Select(r => Path.Combine(AssemblyDirectoryHelper.GetCurrentDirectory(), r))),
+            ReferenceCopyLocalPaths = references.Select(r => Path.Combine(AssemblyDirectoryHelper.GetCurrentDirectory(), r)).ToList(),
             AssemblyResolver = new TestAssemblyResolver()
         };
+
+        if (!Path.IsPathRooted(assemblyPath))
+        {
+            assemblyPath = Path.Combine(AssemblyDirectoryHelper.GetCurrentDirectory(), assemblyPath);
+        }
+
         return weavingTask.ExecuteTestRun(assemblyPath,
             assemblyName: assemblyName,
             ignoreCodes: new []{ "0x80131869" },
