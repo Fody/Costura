070fad640c4501086a879903642ce12f57dd66ef
Geert van Horrik <geert@catenalogic.com>
2020-07-03 09:12:17 +0200

    Simplify dependencies for integration test

--- src/Costura.Fody.IntegrationTests/Integration.cs
+++ src/Costura.Fody.IntegrationTests/Integration.cs
@@ -14,18 +14,15 @@ public class Integration
         Assert.AreEqual("{\"SomeProperty\":\"Test\"}", json);
 
         // just use some code to ensure assemblies are properly embedded.
-        var now = DateTime.Now;
-        var x = TomsToolbox.Core.DateTimeOperations.Max(now, DateTime.Today);
-        Assert.AreEqual(x, now);
+        var now = Catel.FastDateTime.Now;
 
-        var embeddedAssembly = typeof(TomsToolbox.Core.DateTimeOperations).Assembly;
+        Console.WriteLine(now);
+
+        var embeddedAssembly = typeof(Catel.FastDateTime).Assembly;
         var thisAssembly = GetType().Assembly;
 
         Assert.IsEmpty(embeddedAssembly.Location);
         // does not work on build server: Assert.Equal(embeddedAssembly.CodeBase, thisAssembly.CodeBase, StringComparer.OrdinalIgnoreCase);
-
-        var embeddedAssembly2 = typeof(System.Windows.Interactivity.DefaultTriggerAttribute).Assembly;
-        Assert.IsEmpty(embeddedAssembly2.Location);
 
         var targetDir = Path.GetDirectoryName(new Uri(thisAssembly.CodeBase).LocalPath);
 
@@ -34,7 +31,7 @@ public class Integration
             .ToArray();
 
         Assert.IsTrue(localCopyFiles.Any(file => file.StartsWith("Newtonsoft", StringComparison.OrdinalIgnoreCase)));
-        Assert.IsFalse(localCopyFiles.Any(file => file.StartsWith("TomsToolbox", StringComparison.OrdinalIgnoreCase)));
+        Assert.IsFalse(localCopyFiles.Any(file => file.StartsWith("Catel.Core", StringComparison.OrdinalIgnoreCase)));
     }
 
     public string SomeProperty { get; set; } = "Test";
