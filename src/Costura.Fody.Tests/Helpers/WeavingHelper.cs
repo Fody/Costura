953fc4fa791dd159e6d74059e60f116e0ff28beb
Simon Cropp <simon.cropp@gmail.com>
2024-01-30 22:13:36 +1100

    Update WeavingHelper.cs

--- src/Costura.Fody.Tests/Helpers/WeavingHelper.cs
+++ src/Costura.Fody.Tests/Helpers/WeavingHelper.cs
@@ -10,7 +10,7 @@ public static class WeavingHelper
     {
         var currentDirectory = AssemblyDirectoryHelper.GetCurrentDirectory();
 
-        using (var weavingTask = new ModuleWeaver
+        using (var weaver = new ModuleWeaver
         {
             Config = XElement.Parse(config),
             References = string.Join(";", references.Select(r => Path.Combine(currentDirectory, r))),
@@ -22,7 +22,8 @@ public static class WeavingHelper
                 assemblyPath = Path.Combine(currentDirectory, assemblyPath);
             }
 
-            return weavingTask.ExecuteTestRun(assemblyPath,
+            return weaver.ExecuteTestRun(
+                assemblyPath,
                 assemblyName: assemblyName,
                 ignoreCodes: new[] { "0x80131869" },
                 runPeVerify: false);
