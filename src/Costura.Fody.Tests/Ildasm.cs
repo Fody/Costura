262a199ca88a883c81dbf7ea617af487a7cba3a5
Geert van Horrik <geert@catenalogic.com>
2024-08-16 16:30:59 +0200

    Ignore Fody warning (for now)

--- src/Costura.Fody.Tests/Ildasm.cs
+++ src/Costura.Fody.Tests/Ildasm.cs
@@ -6,7 +6,9 @@ public class Ildasm
 {
     public static string Decompile(string afterAssemblyPath, string costuraAssemblyLoader)
     {
+#pragma warning disable CS0618 // Type or member is obsolete
         var decompile = Fody.Ildasm.Decompile(afterAssemblyPath, costuraAssemblyLoader);
+#pragma warning restore CS0618 // Type or member is obsolete
         var checksumPattern = new Regex(@"^(\s*IL_[0123456789abcdef]{4}:  ldstr\s*"")[0123456789ABCDEF]{32,40}""", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
         return string.Join(Environment.NewLine, decompile.Split(new[]{ Environment.NewLine }, StringSplitOptions.None)
             .Where(l => !l.StartsWith("// ", StringComparison.CurrentCulture) && !string.IsNullOrEmpty(l))
